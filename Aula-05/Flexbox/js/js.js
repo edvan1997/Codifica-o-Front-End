478bfe27b4951f3ad8f9d96dcc3d52b22aa98d55
edvan1997 <edvinci0000@gmail.com>
2022-01-25 21:29:14 -0300

    ver01

---
+++ Aula-05/Flexbox/js/js.js
@@ -0,0 +1,8 @@
+// let caixas = document.getElementsByClassName("caixa");
+
+// function mouseOver(){
+//     caixas.style.background="black";
+// }
+// function mouseOut(){
+//     caixas.style.background="#a1c3ff";
+// }
