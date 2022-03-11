d849c478aefdad7e22605bfe42d99a714c74446b
edvan1997 <edvinci0000@gmail.com>
2022-03-10 21:39:32 -0300

    10/03/22

--- Aula-06/js/javascript1.js
+++ Aula-06/js/javascript1.js
@@ -7,10 +7,27 @@
 //     });
 // })
 var ativo = 0
-var modeSwitch = document.querySelector('.mode-switch')
+var modeSwitch = document.querySelector('.noturno')
 if(ativo == 0){
     modeSwitch.addEventListener('click', function(){
-        modeSwitch.classList.add('active')
+        document.documentElement.classList.toggle('dark')
+        document.querySelector('body').classList.toggle('active')
+        document.querySelector('.noturno').classList.toggle('active')
+        document.querySelector('.right-side').classList.toggle('active')
+        document.querySelector('.left-side').classList.toggle('active')
+        document.querySelector('.top-nav-bar').classList.toggle('active')
+        document.querySelector('.links').classList.toggle('active')
+        document.querySelector('.boxes').classList.toggle('active')
+        document.querySelector('.buscador').classList.toggle('active')
+        var icone =document.querySelectorAll('.side-icons')
+        for(i = 0; i < icone.length; i++){
+            icone[i].classList.toggle('active')
+        }
+        
+        
+        
+        
+        
     });
     ativo = 1
 } else {
